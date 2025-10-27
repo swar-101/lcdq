c5eba9c992dced55de701fd0ad135a93c1eed64f
swar-101 <kunwarswarprakash@gmail.com>
2025-10-27 16:40:45 +0530

    Add 3397

---
+++ src/solutions/com/leetcode/problem3397/v1/Solution.java
@@ -0,0 +1,15 @@
+package com.leetcode.problem3397.v1;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int nextFree = Integer.MIN_VALUE;
+        int count = 0;
+        for (int x : nums) {
+
+        }
+    }
+}
